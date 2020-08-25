909588018ea7afebe1f0e4b0bcbb368884ff6b6f
Nathan Stiff <54955862+nstiff23@users.noreply.github.com>
2020-08-25 01:22:10 -0400

    Integrated initiative.py into the bot

--- artifice.py
+++ artifice.py
@@ -4,64 +4,37 @@ import subprocess
 import discord
 from dotenv import load_dotenv
 
+from initiative import Initiative
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-class InitMember:
-    user = None
-    name = ""
-    init = 0
+def roll_dice(user_roll):
+    user_roll = user_roll.replace(" ","")
+    if user_roll[:3] == "adv":
+        res1 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
+        res2 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
+        return max(int(res1.stdout.strip()), int(res2.stdout.strip()))
+    elif user_roll[:3] == "dis":
+        res1 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
+        res2 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
+        return min(int(res1.stdout.strip()), int(res2.stdout.strip()))
+    else:
+        res = subprocess.run(["./dice", user_roll], capture_output=True, text=True) 
+        return int(res.stdout.strip())
 
-    def __init__(self, user, name, init):
-        self.user = user
-        self.name = name
-        self.init = init
-
-    def get_init(self):
-        return self.init
-
-class InitTracker:
-    tracker = []
-    init_counter = 0
-
-    def add(self, name, user, init):
-        member = InitMember(user, name, init)
-        self.tracker.append(member)
-        self.tracker.sort(key=InitMember.get_init)
-
-    def remove(self, name):
-        for i in range(0, len(self.tracker)):
-            member = self.tracker[i]
-            if member.name == name:
-                self.tracker.pop(i)
-                break
-
-    def clear(self):
-        self.tracker = []
-
-    def print_init(self):
-        msg = "Initiative:\n"
-        for member in self.tracker:
-            msg += " " + str(member.init) + " " + member.name + "\n"
-        return msg
-
-    def next(self):
-        member = self.tracker[self.init_counter]
-        self.init_counter += 1
-        if self.init_counter >= len(self.tracker):
-            self.init_counter = 0;
-        return member
+def is_number(string):
+    try:
+        int(string)
+        return True
+    except ValueError:
+        return False
 
 class ArtificeClient(discord.Client):
-    sleeping = False
-    focused = False
-    channel = 0
+    trackers = {}
+    init_msg = {}
+
     past_rolls = {}
-
-    tracker = InitTracker()
-
-    init_msg = None
-    in_init = False
 
     helpmsg = (
             '**Artifice**\n'
@@ -81,19 +54,54 @@ class ArtificeClient(discord.Client):
             )
 
     @staticmethod
-    def roll_dice(user_roll):
-        user_roll = user_roll.replace(" ","")
-        if user_roll[:3] == "adv":
-            res1 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
-            res2 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
-            return max(int(res1.stdout.strip()), int(res2.stdout.strip()))
-        elif user_roll[:3] == "dis":
-            res1 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
-            res2 = subprocess.run(["./dice", user_roll[3:]], capture_output=True, text=True)
-            return min(int(res1.stdout.strip()), int(res2.stdout.strip()))
+    def print_entity(self, entity, current=FALSE):
+        out = ""
+        if current:
+            out += ">"
         else:
-            res = subprocess.run(["./dice", user_roll], capture_output=True, text=True) 
-            return int(res.stdout.strip())
+            out += " "
+
+        roll = entity.roll
+        if roll < 10:
+            out += " " + str(roll)
+        else:
+            out += str(roll)
+
+        name = entity.name[0:20]
+        out += name
+
+        if current:
+            out += "**"
+        out += '\n'
+        return out
+
+    @staticmethod
+    def print_init(self, channel):
+        out = "```\n"
+        tracker = trackers[channel]
+
+        curr, entities = tracker.view()
+        surprise = tracker.surprise
+        lost = tracker.lost
+
+        if len(surprise) > 0:
+            out += "SURPRISE\n========\n"
+            for i in range(0,len(surprise)):
+                out += self.print_entity(surprise[i])
+        out += "INITIATIVE\n==========\n"
+        for i in range(0,len(entities)):
+            bold = (curr == i)
+            out += self.print_entity(entities[i], current=bold)
+        if len(lost) > 0:
+            out += "LOST\n====\n"
+            for i in range(0,len(lost)):
+                out += self.print_entity(lost[i])
+
+        out += "```"
+    
+    @staticmethod
+    def in_init(self, channel):
+        return channel in trackers
 
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
@@ -103,56 +111,75 @@ class ArtificeClient(discord.Client):
             return
         
         if message.content:
-            elif message.content == '!help':
+            if message.content == '!help':
                 await message.author.create_dm()
                 await message.author.dm_channel.send(self.helpmsg)
             elif message.content.split(" ")[0] == '!roll':
                 user_roll = message.content[6:]
                 self.past_rolls[message.author.id] = user_roll
                 roll = self.roll_dice(user_roll)
-                await message.channel.send(message.author.display_name + " rolled " + user_roll + ": " + str(roll))
+                await message.channel.send(message.author.display_name + 
+                        " rolled " + user_roll + ": " + str(roll))
+
             elif message.content == '!re':
                 if message.author.id in self.past_rolls:
                     user_roll = self.past_rolls[message.author.id]
                     roll = self.roll_dice(user_roll)
-                    await message.channel.send(message.author.display_name + " rolled " + user_roll + ": " + str(roll))
+                    await message.channel.send(message.author.display_name + 
+                            " rolled " + user_roll + ": " + str(roll))
                 else:
                     await message.channel.send("No past roll on record!")
+
             elif message.content.split(" ")[0] == "!init":
                 init_command = message.content.split(" ")
                 if init_command[1] == "start" or init_command[1] == "begin":
-                    if self.in_init:
+                    if self.in_init(message.channel):
                         await message.channel.send("Already in initiative!")
                     else:
-                        self.tracker.clear()
-                        await message.channel.send("Initiative begun! Use !init add <bonus> to roll initiative.")
-                        self.init_msg = await message.channel.send(self.tracker.print_init())
-                        await self.init_msg.pin()
-                        self.in_init = True
+                        self.trackers[message.channel] = Initiative()
+                        await message.channel.send("Initiative begun!" + 
+                                " Use !init add <bonus> to roll initiative.")
+                        self.init_msg[message.channel] = await message.channel.send(
+                                self.print_init(message.channel))
+                        await self.init_msg[message.channel].pin()
+
                 elif init_command[1] == "add":
-                    if self.in_init:
-                        init_roll = self.roll_dice("1d20+("+init_command[3]+")")
-                        self.tracker.add(init_command[2], message.author, init_roll)
-                        await self.init_msg.edit(content=self.tracker.print_init())
+                    if self.in_init(message.channel):
+                        adv = 0
+                        if init_command[4] == "adv":
+                            adv = 1
+                        elif init_command[4] == "dis":
+                            adv = -1
+                        
+                        if is_number(init_command[4]):
+                            self.trackers[message.channel].add(init_command[2], int(init_command[3]),
+                                    roll=int(init_command[4]), id=message.author)
+                        else:
+                            self.trackers[message.channel].add(init_command[2], int(init_command[3]),
+                                    id=message.author)
+                        await self.init_msg[message.channel].edit(
+                                content=self.print_init(message.channel))
                     else:
                         await message.channel.send("Not in initiative")
+
                 elif init_command[1] == "remove":
-                    if self.in_init:
-                        self.tracker.remove(init_command[2])
-                        await self.init_msg.edit(content=self.tracker.print_init())
+                    if self.in_init(message.channel):
+                        self.trackers[message.channel].remove(init_command[2])
+                        await self.init_msg.edit(content=self.print_init(message.channel))
                     else: 
                         await message.channel.send("Not in intiative")
+
                 elif init_command[1] == "end":
-                    if self.in_init:
-                        self.tracker.clear()
+                    if self.in_init(message.channel):
+                        self.trackers[message.channel] = None
                         await self.init_msg.unpin()
                         await message.channel.send("Initiative ended")
-                        self.in_init = False
                     else:
                         await message.channel.send("Not in intiative")
+
                 elif init_command[1] == "next":
-                    if self.in_init:
-                        member = self.tracker.next()
+                    if self.in_init(message.channel):
+                        member = self.trackers[message.channel].next()
                         mention = member.user.mention
                         await message.channel.send("Up next: " + member.name + " " + mention)
                     else: 
