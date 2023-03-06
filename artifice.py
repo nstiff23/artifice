27c11f0d3af109a086c95fbf061284e711c5ff70
nas <nathanstff@gmail.com>
2023-03-06 09:34:48 -0700

    moved downloads to music folder

--- artifice.py
+++ artifice.py
@@ -29,7 +29,8 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0', # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'outtmpl': 'music/%(id)s_%(title)s.%(ext)s',
 }
 
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
